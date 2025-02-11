bcafbbde8de4bbff91fcdbfa371a11617c6891c7
GabrielCoh <9924@holbertonstudents.com>
2025-02-11 18:34:15 +0100

    asked julie and pablo for some help

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Basic serialization module that adds the functionality to serialize a Python dictionary to a JSON file and deserialize the JSON file to recreate the Python Dictionary"""
+
+
+import json
+
+
+def serialize_and_save_to_file(date, filename):
+    """Serialize a Python dictionnary to a JSON file"""
+    with open(filename, "w+", encoding="utf-8") as file:
+        file.write(json.dumps(data))
+    pass
+
+
+def load_and_deserialize(filename):
+    """Deserialize the JSON file to recreate the Python dictionnary"""
+    with open(filename, "r", encoding="utf-8") as file:
+        loaded_data = json.loads(file.read())
+    return loaded_data
+    pass
