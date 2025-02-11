5e1187ed893bf4aea7e4918efd75ee3f54d4cfff
GabrielCoh <9924@holbertonstudents.com>
2025-02-11 20:39:00 +0100

    changed some things

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,14 +9,15 @@ import json
 
 def serialize_and_save_to_file(date, filename):
     """Serialize a Python dictionnary to a JSON file"""
-    with open(filename, "w+", encoding="utf-8") as file:
-        file.write(json.dumps(data))
+    with open(filename, "w+", encoding="utf-8") as f:
+        data= json.dumps(data)
+        f.write(data)
     pass
 
 
 def load_and_deserialize(filename):
     """Deserialize the JSON file to recreate the Python dictionnary"""
-    with open(filename, "r", encoding="utf-8") as file:
-        loaded_data = json.loads(file.read())
-    return loaded_data
+    with open(filename, "r", encoding="utf-8") as f:
+        data = f.read()
+        return json.loads(data)
     pass
