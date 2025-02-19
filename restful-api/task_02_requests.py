339fa4c98508067ba848e7ce0f6bba2ea958c1d6
GabrielCoh <9924@holbertonstudents.com>
2025-02-19 01:00:58 +0100

    i am trying anything

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
-"""Consummingand processing data from an API usng Python"""
-
-
+"""Consuming and processing data from an API using Python"""
 import requests
 import csv
 
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
+status_code = response.status_code
+posts = response.json()
+
 
 def fetch_and_print_posts():
-    """Function that fetches posts from JSONPlaceHolder using requests.get()"""
-    r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    if r.status_code == 200:
-        print("Status Code: {}".format(r.status_code))
-        posts = r.json()
+    """Function that fetches all post from JSONPlaceHolder"""
+    print("Status Code: {}".format(status_code))
+    if status_code == 200:
         for post in posts:
-            print(post['title'])
+            print(post["title"])
 
-    else:
-        print("Failed to fetch posts.Status code: {}".format(r.status_code))
 
-    def fetch_and_save_posts():
-        """Function that fetches all post from JSONPlaceHolder"""
-        r = requests.get('https://jsonplaceholder.typicode.com/posts')
-        if r.status_code == 200:
-            posts = r.json()
-            data_to_save = [{'id': post['id'], 'title': post['title'],
-                            'body': post['body']} for post in posts]
-
-            with open('posts.csv', 'w', newline='', encoding='utf-8') as
-            csvfile:
-                fieldnames = ['id', 'title', 'body']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-                writer.writerheader()
-                writer.writerows(data_to_save)
-        else:
-            print("Failed to fetch posts. Status code: {}".format
-                  (r.status_code))
-
-    if __name__ == '__main__':
-        fetch_and_print_posts()
-        fetch_and_save_posts()
+def fetch_and_save_posts():
+    """Function that fetches all post from JSONPlaceholder"""
+    if status_code == 200:
+        with open("posts.csv", "w", encoding='utf-8', newline='') as csvFile:
+            fieldnames = ["id", "title", "body"]
+            csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            csvWriter.writeheader()
+            for post in posts:
+                csvWriter.writerows([{key: post[key] for key in fieldnames}])
