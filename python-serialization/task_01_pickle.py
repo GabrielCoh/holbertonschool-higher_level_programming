6b0cdefa7c9a70fc92794e61d4df22e840d5c26d
GabrielCoh <9924@holbertonstudents.com>
2025-03-28 09:13:54 +0100

    Update task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,40 +1,72 @@
-#!/usr/bin/python3
-"""Serialize and deserialize custom Python objects using the pickle module"""
+#!/usr/bin/env python3
+"""
+This module defines a class CustomObject and demonstrates
+basic serialization using pickle.
+
+"""
 
 
 import pickle
 
 
 class CustomObject:
-    """Custom Python Class"""
+    """
+    A class representing a custom object.
+    """
+
     def __init__(self, name, age, is_student):
+        """
+        Initialize a CustomObject instance.
+
+        Args:
+            name (str): The name of the person.
+            age (int): The age of the person.
+            is_student (bool): Whether the person is a student.
+        """
+
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """The display used to print out the object's attributes"""
-        for key in self.__dict__:
-            print("{} : {}".format(key.capitalize(), self.__dict__[key]))
+        """
+        Display the attributes of the CustomObject instance.
+        """
+
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Serialize the Python dictionnary to the JSON file"""
-        try:
-            with open(filename, "wb")as file:
-                pickle.dump(self, file)
-            except (FileNotFoundError, pickle.PickleError) as e:
-                print("Serialization error: {}".format(e))
-                return None
+        """
+        Serialize the CustomObject instance to a file.
+
+        Args:
+            filename (str): The name of the file to save
+            the serialized object to.
+        """
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize the JSON file to recreate the Python dictionnary"""
+        """
+        Deserialize a CustomObject instance from a file.
+
+        Args:
+            filename (str): The name of the file to load
+            the serialized object from.
+
+        Returns:
+            CustomObject: The deserialized CustomObject instance.
+        """
         try:
-            with open(filename, "rb") as file:
-                obj = pickle.load(file)
-                if not isinstance(obj, cls):
-                    raise TypeError("Object is not of type CustomObject")
-                return obj
-            except (FileNotFoundError, pickle.UnpicklingError, EOFError) as e:
-                print("Deserialization error: {}".format(e))
-                return None
+
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            print("File not found")
+            return None
+        except EOFError:
+            print("Reached end of file")
+            return None
